ce59a9d518a4a4052d46401f78a7b072986f4d81
Undead34 <maizogabriel@gmail.com>
2023-11-15 12:51:52 -0400

    update

--- app/template/components/Options.tsx
+++ app/template/components/Options.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 
 export default function Options() {
   return (
-    <div role="button" tabIndex={0} className={styles["options-container"]}>
+    <div role="button" tabIndex={0} className={styles["options-container"]}
+    >
       <div className={styles["options-text"]}>
         <Image width={32} height={32} src={Key} alt="Option Key" />
         <div className="pl-3 text-[15px]">Opciones de inicio de sesión</div>
